f4bf7a3db48050d76e9b5884b14dd4328212b0bb
matthewgeng <matthewgeng2@gmail.com>
2020-08-22 21:13:19 -0400

    typo fix

--- backend_express/app.js
+++ backend_express/app.js
@@ -104,7 +104,7 @@ app.post('/upload', upload.single('video'), function (req, res) {
                     let videoAnalyzerTransform = {
                         odatatype: "#Microsoft.Media.VideoAnalyzerPreset",
                         audioLanguage: null,
-                        insightsToExtract: "AudioInsightsOnly4",
+                        insightsToExtract: "AudioInsightsOnly",
                         presetName: "VideoAnalyzerPreset"
                     };
 
